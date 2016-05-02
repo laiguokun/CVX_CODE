9ba02a2d17de652e232bde389ad0a2b7b26240d3
GUOKUN LAI <laiguokun@163.com>
2016-05-02 19:39:25 +0800

    Steepest Descent

--- SteepestDescent/SD_backtrack.m
+++ SteepestDescent/SD_backtrack.m
@@ -1,15 +1,17 @@
 % The funciton is f(x) = 0.5 * (x(1) ^ 2 + gamma * x(2) ^ 2)
-function [] = GD_exact()
+function [] = SD_backtrack()
 
 x0 = [10,1];
 gamma = 10;
 x = x0;
 gap = f(x, gamma);
 disp(gap);
+P = [1,0;0,100];
 cnt = 0;
 while (gap > 1e-6 && cnt < 20)
     df = fg(x, gamma);
-    x = exact_line_search(x, -df, gamma);
+    dx = -(P^-1 * df')'; 
+    x = backtrack_line_search(x, df, dx, gamma, 0.3, 0.5);
     gap = f(x,gamma); 
     disp(gap);
     cnt = cnt + 1;
@@ -24,8 +26,11 @@ function g = fg(x,gamma)
     g = [x(1), gamma * x(2)];
 end
 
-function x = exact_line_search(x0, dx, gamma)
-    t = (-gamma * dx(2) * x0(2) - dx(1) * x0(1))/ (dx(1)^2 + gamma * dx(2)^2);
+function x = backtrack_line_search(x0, df, dx, gamma, alpha, beta)
+    t = 1;
+    while (f(x0 + t * dx, gamma) > f(x0, gamma) + alpha * t * df * dx')
+        t = t * beta;
+    end;
     x = x0 + t * dx;
 end
 
