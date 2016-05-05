41433e08d3d2801e33ea5bfeca48005a04286dc9
GUOKUN LAI <laiguokun@163.com>
2016-05-05 22:29:59 +0800

    interiorpoint method via infeasible start

--- NewtonMethod/interiorpoint_infeasible.m
+++ NewtonMethod/interiorpoint_infeasible.m
@@ -1,73 +1,160 @@
 % The funciton is f(x) = exp(x1 + 3*x2 - 0.1) + exp(x1-3*x2-0.1) +
 % exp(-x1-0.1)
-% under inequality Ax + b < 0 A and b is random
-function [] = interiorpoint()
+% under inequality Ax + b > 0 A and b is random
+% under equality A1x = b1
+function [] = interiorpoint_infeasible()
 
 n = 2;
 p = 1;
 res = 2.55927;
 global A;
-A = rand(p,n);
+%A = rand(p,n);
+A = [1,1];
 global b;
-b = rand(p,1);
+%b = rand(p,1);
+b = [0];
+x = zeros(2,1);
 %%find feasible start point
-x = A^-1*(-b);
-if (A*(x+1) + b < 0)
-    x = x + 1;
-else
-    x = x - 1;
+x = [-1;-1];
+
+%stage 1 find feasible start point
+%maximize s == minimize -s
+
+s = min(A * x + b) - 1;
+disp(s);
+x = [s;x];
+t = abs(s);
+mu = 10;
+while (x(1) < 1)
+    x = newton_stage1(t,x);
+    t = mu * t;
 end
-t = f(x,0);
-disp(f(x,0) - res);
+disp(A * x(2:3) + b);
+%stage 2 
+
+res = 2.55927;
+x = x(2:3);
+disp(origin_f(x) - res);
+t = abs(origin_f(x));
 mu = 10;
 while (p/t > 1e-10)
     x = newton(t,x);
     t = mu * t;
-    break;
 end
+disp(origin_f(x) - res);
 end
 
-function newton(t,x)
+function x = newton_stage1(t,x0)
+    x = x0;
     lambda = 1;
-    disp(f(x,t));
+    while(lambda > 1e-10 || x(1) < 1)
+        df = fg_stage1(x,t);
+        ddf = Hession_stage1(x,t);
+        P = ddf;
+        if (det(P) == 0)
+           dx = -df;
+        else
+           dx = P \ -df;
+        end
+        lambda = (dx' * ddf * dx) / 2;
+        x = backtrack_line_search_stage1(x, df, dx, 0.01, 0.5, t);  
+    end
+end
+
+function value = f_stage1(x, t)
+    value = -x(1);
+    value = value * t - sum(fie_stage1(x));
+end
+
+function value = fie_stage1(x)  %function of inequality
+    global A;
+    global b;
+    value = log(A * x(2:3) + b - x(1));
+end
+
+function g = fg_stage1(x, t)
+    g = [-1;0;0];
+    g = t * g - fgie_stage1(x);
+end
+
+function g = fgie_stage1(x) %gradient function of inequality
+    global A;
+    global b;
+    F = A * x(2:3) + b - x(1);
+    g = F.^-1;
+    g = g * [-1;sum(A, 1)'];
+end
+
+function x = backtrack_line_search_stage1(x0, df, dx, alpha, beta, tt)
+    global A;
+    global b;
+    t = 1;
+    while (((A * (x0(2:3) + t * dx(2:3)) + b - (x0(1) + t * dx(1))) < 0) || f_stage1(x0 + t * dx, tt) > f_stage1(x0, tt) + alpha * t * df' * dx)
+        t = t * beta;
+    end;
+    x = x0 + t * dx;
+end
+
+function P = Hession_stage1(x, t)
+    P = zeros(3,3);
+    P = t * P - Hessionie_stage1(x);
+end
+
+function P = Hessionie_stage1(x) %Hession of inequality
+    global A;
+    global b;
+    F = A*x(2:3) + b - x(1);
+    df = fgie_stage1(x);
+    P = -(df/F) * [-1;sum(A,1)']';
+end
+
+
+function x = newton(t,x0)
+    x = x0;
+    lambda = 1;
+    cnt = 0;
     while(lambda > 1e-10)
         df = fg(x,t);
         ddf = Hession(x,t);
         P = ddf;
-        dx = P^-1 * -df;
-        lambda = dx' * ddf * dx;
-        x = backtrack_line_search(x, df, dx, 0.01, 0.5, t);   
-        disp(f(x));
-        break;
+        dx = P \ -df;
+        lambda = (dx' * ddf * dx) / 2;
+        x = backtrack_line_search(x, df, dx, 0.01, 0.5, t);  
     end
 end
 
+function value = origin_f(x)
+    value = (exp(x(1) + 3*x(2) - 0.1) + exp(x(1)-3*x(2)-0.1) + exp(-x(1) - 0.1));
+end
 function value = f(x, t)
     value = (exp(x(1) + 3*x(2) - 0.1) + exp(x(1)-3*x(2)-0.1) + exp(-x(1) - 0.1));
-    value = value * t + sum(fie(x));
+    value = value * t - sum(fie(x));
 end
 
-function value = fie(x) %function of inequality
+function value = fie(x)  %function of inequality
     global A;
     global b;
-    value = A * x + b;
+    value = log(A * x + b);
 end
 
 function g = fg(x, t)
     g = [exp(x(1) + 3*x(2) - 0.1) + exp(x(1)-3*x(2)-0.1) - exp(-x(1) - 0.1); 3 * exp(x(1) + 3*x(2) - 0.1) - 3 * exp(x(1)-3*x(2)-0.1)];
-    g = t * g + fgie(x);
+    g = t * g - fgie(x);
 end
 
 function g = fgie(x) %gradient function of inequality
     global A;
     global b;
-    g = f(x).^-1;
-    g = g .* sum(A)';
+    F = A * x + b;
+    g = F.^-1;
+    g = g * sum(A, 1)';
 end
 
 function x = backtrack_line_search(x0, df, dx, alpha, beta, tt)
+    global A;
+    global b;
     t = 1;
-    while (f(x0 + t * dx, tt) > f(x0, tt) + alpha * t * df' * dx)
+    while ((min(A * (x0 + t * dx) + b) < 0) || f(x0 + t * dx, tt) > f(x0, tt) + alpha * t * df' * dx)
         t = t * beta;
     end;
     x = x0 + t * dx;
@@ -79,16 +166,14 @@ function P = Hession(x, t)
     P(1,2) = 3 * exp(x(1) + 3*x(2) - 0.1) - 3 * exp(x(1)-3*x(2)-0.1);
     P(2,1) = P(1,2);
     P(2,2) = 9 * exp(x(1) + 3*x(2) - 0.1) + 9 * exp(x(1)-3*x(2)-0.1);
-    P = t * P + Hessionie(x);
+    P = t * P - Hessionie(x);
 end
 
 function P = Hessionie(x) %Hession of inequality
-    P = zeros(2,2);
-    f = fie(x);
+    global A;
+    global b;
+    F = A * x + b;
     df = fgie(x);
-    P(1,1) = -df(1)/f(1) * df(1);
-    P(1,2) = -df(1)/f(1) * df(2);
-    P(2,1) = -df(2)/f(2) * df(1);
-    P(2,2) = -df(2)/f(2) * df(2);
+    P = -(df/F) * sum(A,1);
 end
 
